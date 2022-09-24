7f5f197d56b3c334f1655619975a6c6dd2be65ce
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-09-24 08:40:23 -0300

    Fix: Page 404 when refreshing

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Header } from '../../components/Header'
 
 import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   const [activeColor, setActiveColor] = useState(false)
+
+  const location = useLocation()
 
   useEffect(() => {
     function scroll() {
